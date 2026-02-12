2f6b4758183544dbd641c406d5ab90873b647368
Link4Electronics <link4electronics@gmail.com>
2026-02-11 23:50:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -13,7 +13,7 @@ export DEPLOY_OPENGL=1
 
 # Deploy dependencies
 quick-sharun /opt/xash3d/*
-mv -v /opt/xash3d/valve/extras.pk3 ./AppDir/bin
+mv -v /opt/xash3d/extras.pk3 ./AppDir/bin
 #mv /opt/xash3d/* ./AppDir/bin
 #echo 'SHARUN_WORKING_DIR=${SHARUN_DIR}/bin' >> ./AppDir/.env
 
